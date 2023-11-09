827175e49678e0fb39a008e48d20fbfe45a9922a
unknown <2482173874@qq.com>
2023-11-09 18:34:04 +0800

    accent_read

--- app/views.py
+++ app/views.py
@@ -5,6 +5,8 @@ import requests
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from mine import final
+import os
 # Create your views here.
 
 @csrf_exempt
@@ -17,7 +19,7 @@ def helloView(request):
             "json": "json",
             "hello": "hello",
         }
-        return JsonResponse(json_obj)
+        return JsonResponse(json_obj, status = 200)
         return render(request, "index.html", {"testUnit": testUnit})
     if request.method == "POST":
         print(request)
@@ -42,6 +44,36 @@ def login(request):
             }
         )
         print(login_response.text)
-        return JsonResponse(json.loads(login_response.text))
+        return JsonResponse(json.loads(login_response.text), status = 200)
     else:
-        return JsonResponse({"code": "405", "message": "Method not allowed"})
+        return JsonResponse({"code": "405", "message": "Method not allowed"}, status = 405)
+    
+@csrf_exempt
+def read(request):
+    if request.method == "POST":
+        json_param = json.loads(request.body)
+        audio = json_param['file']
+        with open("./mine/hi.m4a", "rb") as ft:
+            audio = ft.read()
+        wave = json_param['wave']
+        openid = json_param['openid']
+        acc_fileName = "./mine/" + openid + "_tmp"
+        with open(acc_fileName + ".mp3", "wb") as f:
+            f.write(audio)
+        result = final.main(acc_fileName + ".mp3", wave)
+        print(result)
+        os.remove(acc_fileName + ".json")
+        os.remove(acc_fileName + ".mp3")
+        os.remove(acc_fileName + ".wav")
+
+        return JsonResponse(
+            {
+                "code": 0,
+                "info": "Success in word analyzing",
+                "result": result
+            },
+            status = 200
+        )
+
+    else:
+        return JsonResponse({"code": "405", "message": "Method not allowed"}, status = 405)
--- koko/urls.py
+++ koko/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.helloView),
     path("login/", views.login),
+    path("read/", views.read),
 ]
