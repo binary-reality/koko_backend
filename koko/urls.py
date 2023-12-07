aa9edba3b2a3116e7027e0704c20afe95793e9b6
unknown <2482173874@qq.com>
2023-12-07 17:57:04 +0800

    feature_friend_uidsearch_uncomfirmed

--- koko/urls.py
+++ koko/urls.py
@@ -42,4 +42,6 @@ urlpatterns = [
 
     path("word/add/", views.word_add, name='word_add'),
     path("word/remove/", views.word_remove, name='word_remove'),
+
+    path("friend/uidsearch", views.friend_uidsearch, name='friend_uidsearch')
 ]
